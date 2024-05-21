b38d218ca5167e8d1e8714f8d1b91d77fcaa9ee0
Omar <omarshaf3y99@gmail.com>
2024-05-21 19:22:55 +0300

    edit some bugs

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/home/presentation/views/widgets/Similar_books_section.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_section.dart';
 
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
